91d786c597d86dfb6f8920a79da39de6f203ffc9
vidm27 <davidmoran464@gmail.com>
2023-03-03 23:51:35 -0500

    settig section style

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import SearchWord from "./components/SearchWord.vue";
 import ResultWord from "./components/ResultWord.vue";
+import Setting from "./components/Setting.vue";
 import 'purecss/build/pure.css';
 </script>
 
 <template>
   <div class="container">
+    <Setting></Setting>
     <SearchWord></SearchWord>
     <ResultWord></ResultWord>
   </div>
@@ -15,5 +17,6 @@ import 'purecss/build/pure.css';
 .container{
   max-width: 737px;
   margin: 0 auto;
+  padding: 0 1em;
 }
 </style>
