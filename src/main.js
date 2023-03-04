59847ed5fcfa2dfb46551e8035368d7329a97d72
vidm27 <davidmoran464@gmail.com>
2023-03-03 22:25:58 -0500

    add icon to config app

--- src/main.js
+++ src/main.js
@@ -1,9 +1,27 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
 import mitt from 'mitt'
 import App from './App.vue'
+import './style.css'
+/* import the fontawesome core */
+import {library} from '@fortawesome/fontawesome-svg-core'
+
+/* import font awesome icon component */
+import {FontAwesomeIcon} from '@fortawesome/vue-fontawesome'
+
+/* import specific icons */
+import {faArrowUpRightFromSquare, faBook, faMoon} from '@fortawesome/free-solid-svg-icons'
+/* add icons to the library */
+const icons = [
+    faArrowUpRightFromSquare,
+    faBook,
+    faMoon
+]
+library.add(icons)
+
 
 const emitter = mitt()
 const app = createApp(App)
 
 app.config.globalProperties.emitter = emitter
+app.component('font-awesome-icon', FontAwesomeIcon)
 app.mount('#app')
